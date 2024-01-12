73f153e0eb1890f8f8535de6b815819fb2b1e96b
atsss <atsushi0623itoh@gmail.com>
2024-01-12 09:40:23 +0000

    Create installer

---
+++ installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# check update
+sudo apt update -y
+sudo apt upgrade -y
+
+# virtualenv
+sudo apt install -y virtualenv python3-virtualenv
+
+# opencv
+sudo apt install -y python3-opencv
+
+# picamera
+sudo apt install -y python3-picamera2
